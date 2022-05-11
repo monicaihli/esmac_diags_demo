0d1d8907016570ca9eed7a1ffec0bf41e3ae7375
Monica Ihli <email@monicaihli.com>
2022-05-11 10:28:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,17 @@
 import setuptools
-setuptools.setup()
+setuptools.setup(
+  name="esmac_diags_demo",
+  version = 1.0.alpha.demo,
+  license="BSD",
+  packages=find_packages(
+    where='src'
+  ),
+  package_dir={"": "src"},
+  install_requires=[
+    "numpy",
+    "pytest",
+    "matplotlib",
+    "netCDF4",
+    "pip"]
+
+)
